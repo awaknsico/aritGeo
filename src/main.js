5f2848ea5153af40e6a00d89e2dfeac2377e3dba
Awak Nsico <awaknsikak@gmail.com>
2017-09-26 05:59:15 +0100

    Optimize Tests and Functions

--- src/main.js
+++ src/main.js
@@ -1,42 +1,65 @@
 'use strict';
 
 module.exports ={
-aritGeo: (list) => {
-	var i = 1;
-	var isArit = true;
-	var isGeo = true;
-	var diff = list[1] - list[0];
-	var rate = list[1] / list[0];
-	var init = list[0];
+	aritGeo: (list) => {
 
-	if(list == false || typeof list== 'boolean' || typeof list == 'number'){
+		var init = list[0];
+		var i = 1;
+		var isArit = true;
+		var isGeo = true;
+		var quot = list[1] - list[0];
+		var rate = list[1] / list[0];
+
+		//Checking for invalid and empty inputs
+		if(list == false || typeof list == 'boolean' || typeof list == 'number'){
 			return 0;
-	}else{
-		while(isArit == true && list[i] != undefined){
-			if(list[i]-init == diff){
-				init = list[i];
+		}
+
+		else{
+			//Handling Arithmetic Progression
+			while(isArit && list[i] != undefined){
+
+				if(list[i]-init == quot){
+					init = list[i];
+					i++;
+				}
+
+				else{
+					isArit = false;
+					init = list[0]
+					i = 1;
+				}
+
+			}
+			//Handling Geometric Progression
+			while(isGeo && list[i] != undefined){
+
+				if(list[i]/init == rate){
+					init = list[i]
+				}
+
+				else{
+					isGeo = false;
+				}
+
 				i++;
-			}else{
-				isArit = false;
-				init = list[0]
-				i = 1;
+
 			}
+
 		}
-		 while(isGeo == true && list[i] != undefined){
-			if(list[i]/init == rate){
-				init = list[i]
-			}else{
-				isGeo = false;
-			}
-			i++
+
+		//Handling return values...
+		//for Arithmetic Progression
+		if(isArit) {
+			return "Arithmetic";
+		}
+		//for Geometric Progression
+		else if(isGeo) {
+			return "Geometric";
+		}
+		//for non-Progressive inputs
+		else{
+			return "-1";
 		}
 	}
-	if(isArit) {
-		return "Arithmetic";
-	} else if(isGeo) {
-		return "Geometric";
-	}else{
-		return "-1";
-	}
-}
 }
--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,7 @@ const myApp = require('../src/main.js');
 
 describe("aritGeo", function(){
 
+
     describe("Handle valid input", function(){
 
         it("Should return 0 for []",function(){
@@ -15,38 +16,6 @@ describe("aritGeo", function(){
 
         it("Should return -1 for [1, 2, 10, 7, 3]",function(){
             assert.equal(myApp.aritGeo([1, 2, 10, 7, 3]),"-1")
-        });
-
-        it("Should return Arithmetic for [5, 10, 15, 20, 25]",function(){
-           assert.equal(myApp.aritGeo([5, 10, 15, 20, 25]),"Arithmetic")
-       });
-
-        it("Should return Arithmetic for [2, 4, 6, 8, 10]",function(){
-            assert.equal(myApp.aritGeo([2, 4, 6, 8, 10]),"Arithmetic")
-        });
-
-        it("Should return Arithmetic for [100, 200, 300, 400, 500]",function(){
-           assert.equal(myApp.aritGeo([100, 200, 300, 400, 500]),"Arithmetic")
-       });
-
-        it("Should return Arithmetic for [1, 2, 3, 4, 5]",function(){
-           assert.equal(myApp.aritGeo([1, 2, 3, 4, 5]),"Arithmetic")
-       });
-
-        it("Should return Geometric for [2, 6, 18, 54, 162]",function(){
-            assert.equal(myApp.aritGeo([2, 6, 18, 54, 162]),"Geometric")
-        });
-
-        it("Should return Geometric for [5, 10, 20, 40, 80]",function(){
-            assert.equal(myApp.aritGeo([5, 10, 20, 40, 80]),"Geometric")
-        });
-
-        it("Should return Geometric for [81, 27, 9, 3, 1]",function(){
-           assert.equal(myApp.aritGeo([81, 27, 9, 3, 1]),"Geometric")
-       });
-
-        it("Should return Geometric for [10,100,1000,10000]",function(){
-            assert.equal(myApp.aritGeo([10,100,1000,10000]),"Geometric")
         });
 
     });
@@ -72,6 +41,46 @@ describe("aritGeo", function(){
         it("Should return 0 for number",function(){
             assert.equal(myApp.aritGeo(4),"0")
         });
+    });
+
+    describe("Handle Arithmetic Progression", function(){
+
+        it("Should return Arithmetic for [5, 10, 15, 20, 25]",function(){
+           assert.equal(myApp.aritGeo([5, 10, 15, 20, 25]),"Arithmetic")
+        });
+
+        it("Should return Arithmetic for [2, 4, 6, 8, 10]",function(){
+            assert.equal(myApp.aritGeo([2, 4, 6, 8, 10]),"Arithmetic")
+        });
+
+        it("Should return Arithmetic for [100, 200, 300, 400, 500]",function(){
+           assert.equal(myApp.aritGeo([100, 200, 300, 400, 500]),"Arithmetic")
+        });
+
+        it("Should return Arithmetic for [1, 2, 3, 4, 5]",function(){
+           assert.equal(myApp.aritGeo([1, 2, 3, 4, 5]),"Arithmetic")
+        });
+
+    });
+
+    describe("Handle Arithmetic Progression", function(){
+
+        it("Should return Geometric for [2, 6, 18, 54, 162]",function(){
+            assert.equal(myApp.aritGeo([2, 6, 18, 54, 162]),"Geometric")
+        });
+
+        it("Should return Geometric for [5, 10, 20, 40, 80]",function(){
+            assert.equal(myApp.aritGeo([5, 10, 20, 40, 80]),"Geometric")
+        });
+
+        it("Should return Geometric for [81, 27, 9, 3, 1]",function(){
+           assert.equal(myApp.aritGeo([81, 27, 9, 3, 1]),"Geometric")
+        });
+
+        it("Should return Geometric for [10,100,1000,10000]",function(){
+            assert.equal(myApp.aritGeo([10,100,1000,10000]),"Geometric")
+        });
+
     });
 
 });
